35da28459ab478f9982333eb60d58acbce1297d2
chenlu <chenlu@globalroam.com>
2016-08-30 13:19:04 +0800

    add popumenulist (仿QQ消息长按菜单选项)

--- MyProject/app/src/main/java/com/future/myproject/MainActivity.java
+++ MyProject/app/src/main/java/com/future/myproject/MainActivity.java
@@ -1,11 +1,14 @@
 package com.future.myproject;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
-import com.future.map.baidu.ui.BaiduMapActivity;
+import com.future.widgets.library.PopupListMenu;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,7 +21,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         butterknife.ButterKnife.bind(this);
 
-        Intent i = new Intent(this, BaiduMapActivity.class);
-        startActivity(i);
+
+    }
+
+    public void tesPopupMenu(View view) {
+        PopupListMenu popupList = new PopupListMenu();
+        List<String> list = new ArrayList<>();
+        list.add("全选");
+        list.add("复制");
+        list.add("更多");
+        popupList.init(this, view, list, new PopupListMenu.OnPopupListClickListener() {
+            @Override
+            public void onPopupListClick(View contextView, int contextPosition, int position) {
+
+            }
+        });
     }
 }
